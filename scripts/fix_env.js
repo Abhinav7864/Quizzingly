cf85a7844fedd60a063b32c46508ac4e0c4000fd
Abhinav7864 <ak1492612@gmail.com>
2026-02-28 12:31:59 +0530

    Pushed with changed

--- scripts/fix_env.js
+++ scripts/fix_env.js
@@ -1,11 +1,1 @@
-import fs from 'fs';
-const envPath = './.env';
-let content = fs.readFileSync(envPath, 'utf8');
-const keyLine = 'GEMINI_API_KEY=AIzaSyClq-VJmbXE48ZtuF4FZ--Z3bB8zsYei_g';
-if (!content.includes('GEMINI_API_KEY')) {
-    content = content.trim() + '\n' + keyLine + '\n';
-} else {
-    content = content.replace(/GEMINI_API_KEY=.*/, keyLine);
-}
-fs.writeFileSync(envPath, content);
-console.log('Fixed .env content');
+
